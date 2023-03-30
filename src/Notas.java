451d145b32927054f5654da500ebce7864af1a5f
gabriel andrade <tisco09090@gmail.com>
2023-03-30 09:28:02 -0400

    comitando arquivos

---
+++ src/Notas.java
@@ -0,0 +1,39 @@
+public class Notas {
+
+    public static void main(String agrs []){
+
+        int nota1 = 10;
+
+        int nota2 = 8;
+
+        int nota3 = 9;
+
+        int nota4 = 5;
+
+        int div = 4;
+
+        int nota5 = nota1+nota2+nota3+nota4;
+
+        int nota6 = nota5/div;
+
+
+        System.out.println("*** Notas ***");
+        System.out.println(nota1);
+        System.out.println(nota2);
+        System.out.println(nota3);
+        System.out.println(nota4);
+        System.out.println("*** Resultado das notas ***");
+        System.out.println(nota6);
+
+
+
+
+    }
+
+
+
+
+
+
+}
+
